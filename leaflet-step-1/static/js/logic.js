c234b25c8d319e75f275a37c2e0637523f6cf2b9
debrawu <dwu1124@gmail.com>
2020-12-07 18:23:10 -0600

    adding more

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -9,21 +9,27 @@ d3.json(queryURL, function(data){
 // d3.json(queryURL, data=>{
     console.log(data.features)
 
-    var earthquakes = L.geoJSON(data.features,
-        {
- //           style: 
-            onEachFeature: addPopup
-        })
+    // var earthquakes = L.geoJSON(data.features,
+//         {
+//  //           style: 
+//             onEachFeature: addPopup
+//         })
     
-    createMap(earthquakes)
+    createMap(data.features);
 //    earthquakes.addTo(myMap)
+    //)
 });
 
-function addPopup(feature, layer){
-    return layer.bindPopup(`<h3> ${feature.properties.place}`)
-}
+// function addPopup(feature, layer){
+//     return layer.bindPopup(`<h3> Magnitude: ${feature.properties.mag}</h3> <p> ${feature.properties.place} </p>`)
+// }
 
-function createMap(layerGroup) {
+// function markerSize(feature) {
+//     return feature.properties.mag * 20
+// }
+
+function createMap(data) {
+
     // Create a map object
     var myMap = L.map("mapid", {
         center: [15.5994, -28.6731],
@@ -37,9 +43,57 @@ function createMap(layerGroup) {
         accessToken: API_KEY
     }).addTo(myMap);
 
-    layerGroup.addTo(myMap)
-    legend.addTo(myMap)
-};
+    // console.log(layerGroup)
+
+    // iterate through the data for each earthquake
+    data.forEach(feature => {
+        // set a variable for magnitude
+        var magnitude = feature.properties.mag;
+        var depth = feature.geometry.coordinates[2]
+        console.log(depth)
+
+        // set a switch statement for color
+        var color = ''
+
+        if (depth <= 100){
+            color = 'lightpink';
+        }
+        else if (depth <= 200){
+            color = 'lightcoral'
+        }
+        else if (magnitude <= 300){
+            color ='deeppink'
+        }
+
+        else if (magnitude <= 400){
+            color = 'indianred'
+        }
+
+        else if (magnitude <= 500) {
+            color = 'firebrick'
+        }
+        else {
+            color = 'darkred'
+        }
+
+        L.circle([feature.geometry.coordinates[0], feature.geometry.coordinates[1]], {
+            color: color,
+            fillColor: color,
+            fillOpacity: 0.75,
+            radius: magnitude*50000
+        }).bindPopup(`<h3> Magnitude: ${feature.properties.mag}</h3> <h3>Depth: ${feature.geometry.coordinates[2]}</h3> <p> ${feature.properties.place} </p>`).addTo(myMap)
+
+
+
+    })
+
+
+
+    // layerGroup.addTo(myMap)
+    // legend.addTo(myMap)
+
+    
+
 
 
 // add the popup
@@ -56,16 +110,25 @@ function createMap(layerGroup) {
 // }
 // include pop ups for each marker when clicked with information 
 
-// create a legend
-var legend=L.control({
+    // create a legend
+    var legend=L.control({
     position: 'bottomright'
-});
+    });
 
-legend.onAdd=function(){
-    legendDiv=L.DomUtil.create('div', 'legend');
-    // create rectangles to represent each "color"
-    // under each rectangle would be the range of values
-    // forEach
-    legendDiv.innerHTML='TEST'
-    return legendDiv;
+    legend.onAdd=function(){
+        legendDiv=L.DomUtil.create('div', 'legend');
+
+        // create rectangles to represent each "color"
+        var colors = ['lightpink', 'lightcoral', 'deeppink', 'indianred', 'firebrick', 'darkred'];
+        // under each rectangle would be the range of values
+        var range = ['<100', '100-200', '200-300', '300-400', '400-500', '500+']
+        // forEach
+        for (var i=0; i <colors.length; i++){
+            legendDiv.innerHTML +=
+            '<li style="background-color:'+ colors [i] + '">' + range[i] + '</li>'
+
+        }
+    
+        return legendDiv;
+    }.addTo(myMap)
 };
